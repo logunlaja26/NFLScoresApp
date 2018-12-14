6995d30f14ae0dd4d3463768fd05aa3ac955c8f7
logunlaja26 <logunlaja26@gmail.com>
2018-12-14 15:45:58 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import csv
+
+app = Flask(__name__)
+
+
+@app.route("/home")
+@app.route("/")
+def csvParser():
+	output = []
+	with open('nfl_games.csv', 'r') as csv_file:
+		csv_reader = csv.DictReader(csv_file)
+		for line in csv_reader:
+		 	output.append(line)
+	return output
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
+	
+	
