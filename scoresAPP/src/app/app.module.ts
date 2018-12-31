a0ea297eebcc028c2cceb6a2500db576856fc953
logunlaja26 <logunlaja26@gmail.com>
2018-12-30 18:38:49 -0600

    app-root loading..

--- scoresAPP/src/app/app.module.ts
+++ scoresAPP/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module'; 
 import { AppComponent } from './app.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
