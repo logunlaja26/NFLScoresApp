ec455a7101aee122843fe919d8e9cff747eaea47
logunlaja26 <logunlaja26@gmail.com>
2019-01-05 16:32:39 -0600

    flask scores api

--- main.py
+++ main.py
@@ -14,14 +14,25 @@ def staticFile(staticFileName):
 	staticFileUrl = url_for('static', filename=staticFileName)
 	return redirect(staticFileUrl)
 
+class Score:
+	def __init__(self,dateOfContest,homeTeam,awayTeam,homeTeamScores,awayTeamScores):
+		self.dateOfContest = dateOfContest
+		self.homeTeam = homeTeam
+		self.awayTeam = awayTeam
+		self.homeTeamScores = homeTeamScores
+		self.awayTeamScores = awayTeamScores
+
+
 @app.route("/api/scores")
 def csvParser():
-	output = []
+	scores = []
 	with open('nfl_games.csv', 'r') as csv_file:
 		csv_reader = csv.DictReader(csv_file)
-		for line in csv_reader:
-		 	output.append(line)
-	return jsonify({'nfl_scores': output})
+		for score in csv_reader:
+			if score['season'] == '2017':
+				gameScore = Score(score['date'],score['team1'],score['team2'],score['score1'],score['score2'])
+				scores.append(gameScore.__dict__)
+	return jsonify(scores)
 
 @app.route("/api/historicalPage")
 def historicalPage():
@@ -29,5 +40,3 @@ def historicalPage():
 
 if __name__ == "__main__":
 	app.run(debug=True)
-	
-	
